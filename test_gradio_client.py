1ef489678bdcc38692071c8e1812e835cb7eb1f0
Kazuki Yoda <yd.tokyu9006f@gmail.com>
2025-06-26 18:17:54 -0700

    get HF api schema as dict

--- test_gradio_client.py
+++ test_gradio_client.py
@@ -1,14 +1,20 @@
+import json
+
 from gradio_client import Client
 
 client = Client("black-forest-labs/FLUX.1-dev")
-result = client.predict(
-    prompt="A beautiful girl with long hair and blue eyes",
-    seed=0,
-    randomize_seed=True,
-    width=1024,
-    height=1024,
-    guidance_scale=3.5,
-    num_inference_steps=28,
-    api_name="/infer"
-)
-print(result)
+api = client.view_api(return_format="dict")
+print(type(api))
+print(json.dumps(api, indent=2))
+
+# result = client.predict(
+#     prompt="A beautiful girl with long hair and blue eyes",
+#     seed=0,
+#     randomize_seed=True,
+#     width=1024,
+#     height=1024,
+#     guidance_scale=3.5,
+#     num_inference_steps=28,
+#     api_name="/infer"
+# )
+# print(result)
